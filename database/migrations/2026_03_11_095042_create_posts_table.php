daa4825f572a767bd156a8320f11cdc9d40bca38
AleksoBre <aleksstevancevic@gmail.com>
2026-03-12 11:42:50 +0100

    added constraints for migrations

--- database/migrations/2026_03_11_095042_create_posts_table.php
+++ database/migrations/2026_03_11_095042_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->text('content');
             $table->timestamps();
         });
