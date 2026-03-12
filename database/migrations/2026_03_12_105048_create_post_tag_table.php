036db44e31cabd3db5a1bfaa550d6b46fd3610a4
AleksoBre <aleksstevancevic@gmail.com>
2026-03-12 11:52:03 +0100

    created migration for post_tag pivot table

--- database/migrations/2026_03_12_105048_create_post_tag_table.php
+++ database/migrations/2026_03_12_105048_create_post_tag_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignIdFor(Post::class);
+            $table->foreignIdFor(Tag::class);
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
     }
 };
