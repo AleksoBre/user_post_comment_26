187040ab3e976ad2722e399c614eb9ea31f0f5f3
AleksoBre <aleksstevancevic@gmail.com>
2026-03-13 10:58:07 +0100

    wrong commit can't be bothered but this one should be for the users page to show paginated users

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,16 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 Route::get('/users', function () {
-    return view('users');
+    return view('users', ['users' => User::withCount('posts', 'comments')->paginate(5)]);
 });
 Route::get('/posts', function () {
     return view('posts');
 });
 
-
-// TO DO:
-// - create models
-
-
-//1 user hasMany posts
-//1 post belongsTo 1 user
-
-//1 user hasMany comments
-//1 comment belongsTo 1 user
-
-//1 post hasMany comments
-//1 comment belongsTo 1 post
-
-//1 tag belongsToMany posts
-//1 post belongsToMany tags OVDE JE PIVOT TABLE
-
-
-
+//loading usera 
