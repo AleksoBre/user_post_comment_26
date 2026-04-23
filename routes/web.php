cd143acc37c0efb2d1739f16aecec01fbcf6307b
AleksoBre <aleksstevancevic@gmail.com>
2026-04-23 16:56:24 +0200

    created mailable class

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,21 @@ use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
+use App\Mail\JobPosted;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
+
+route::get('/mail', function() {
+    Mail::to('aleksstevancevic@gmail.com')->send(
+        new JobPosted()
+    );
+
+    return 'done';
+});
+
+
 
 //Auth
 Route::middleware('guest')->group(function() {
@@ -18,7 +30,6 @@ Route::middleware('guest')->group(function() {
     Route::post('/login', [SessionController::class, 'store'])->name('login.store');
     });
 Route::delete('/session', [SessionController::class, 'destroy'])->middleware('auth');
-    
 
 Route::controller(UserController::class)->group(function() {
     Route::get('/users', 'index')->name('users.index');
@@ -35,7 +46,7 @@ Route::controller(PostController::class)->group(function() {
     Route::middleware('auth')->group(function () {
         Route::get('/posts/create', 'create')->name('posts.create');
         Route::post('/posts', 'store')->name('posts.store');
-        
+
         Route::get('/posts/{post}/edit', 'edit')->can('edit-post', 'post')->name('posts.edit');
         Route::patch('/posts/{post}', 'update')->can('edit-post', 'post')->name('posts.update');
         Route::delete('/posts/{post}', 'destroy')->can('delete-post', 'post')->name('posts.destroy');
